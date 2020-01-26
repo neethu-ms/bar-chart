cd12ea68b564773a346a888ad1139430d783bf08
neethu-ms <msneethu2989@gmail.com>
2020-01-26 21:10:41 +0000

    Added code to avoid overlapping of properties of different bar charts

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,18 +1,26 @@
 
+let barCount = 0;
 // Function to draw single valued bar chart starts
 function drawSingleValuedBarChart(data, options, element) {
   let spaceWidth = options.barSpacing;
   let valueCount = data.length;
   let width = options.width;
   let height = options.height;
-  let newElement = "<div><h1 class=\"barTitle\">" + options.barTitle + "</h1><div class=\"barChart\">";
-  newElement += "<h2 class=\"yaxis\">" + options.yAxis + "</h2>";
+  let tickLabelLeftMargin = -90;
+
+
+  let prefix = "A" + barCount;
+  console.log("Prefix=" + prefix);
+  barCount++;
+
+  let newElement = "<div><h1 class=\"" + prefix + "barTitle\">" + options.barTitle + "</h1><div class=\"" + prefix + "barChart\">";
+  newElement += "<h2 class=\"" + prefix + "yaxis\">" + options.yAxis + "</h2>";
 
   let tickCount = options.tickValues.length;
 
   //Creating tick marks using horizontal line
   for (let i = 0; i < tickCount; i++) {
-    newElement += "<h2 class=\"tickValue" + i + "\">" + options.tickValues[i] + "</h2><hr class=\"hr" + i + "\">";
+    newElement += "<h2 class=\"" + prefix + "tickValue" + i + "\">" + options.tickValues[i] + "</h2><hr class=\"" + prefix + "hr" + i + "\">";
   }
 
   // To align elements as a row using the class flexRow
@@ -23,15 +31,21 @@ function drawSingleValuedBarChart(data, options, element) {
 
   // Finding maximum value so that bar height can be adjusted according to that value
   let maxValue = getMaxValue(data);
+  if (maxValue >= 100 && maxValue < 1000) {
+    tickLabelLeftMargin = -165;
+  } else if (maxValue > 1000) {
+    tickLabelLeftMargin = -185;
+  }
 
   for (let i = 0; i < data.length; i++) {
     //using flexColumn to arrange elements as a column
     newElement += "<div class=\"flexColumn\">";
-    newElement += "<div class=\"bar bar" + i + "\"><p>" + data[i].value + "</p></div><h2>" + data[i].label + "</h2></div>" + "<div class=\"space\"></div>";
+    newElement += "<div class=\"" + prefix + "bar" + " " + prefix + "bar" + i + "\"><p>" + data[i].value + "</p></div><h2>" +
+      data[i].label + "</h2></div>" + "<div class=\"" + prefix + "space\"></div>";
   }
   newElement += "</div>";
   newElement += "</div>";
-  newElement += "<br><br><br><h2 class=\"xaxis" + "\">" + options.xAxis + "</h2>";
+  newElement += "<h2 class=\"" + prefix + "xaxis" + "\">" + options.xAxis + "</h2>";
   newElement += "</div>";
 
 
@@ -39,7 +53,7 @@ function drawSingleValuedBarChart(data, options, element) {
   $(element).append(newElement);
 
   // drawing bar chart by setting css properties
-  $(".barChart").css({
+  $("." + prefix + "barChart").css({
     "width": width + "px", "height": height + "px", "border-left": "2px solid black",
     "border-bottom": "2px solid black", "display": "inline-block"
   });
@@ -51,19 +65,19 @@ function drawSingleValuedBarChart(data, options, element) {
     //computing bar height based on the maximum value
     let barHeight = (data[i].value / maxValue) * height;
 
-    $(".bar" + i).css({ "height": barHeight + "px", "margin-top": (height - barHeight) + "px" });
+    $("." + prefix + "bar" + i).css({ "height": barHeight + "px", "margin-top": (height - barHeight) + "px" });
   }
 
-  $(".bar").css({ "width": barWidth + "px", "background-color": options.barColor, "border": "1px solid black;" });
-  $(".space").css("width", spaceWidth + "px");
+  $("." + prefix + "bar").css({ "width": barWidth + "px", "background-color": options.barColor, "border": "1px solid black" });
+  $("." + prefix + "space").css("width", spaceWidth + "px");
 
-  $("barChart").css("position", "relative");
-  $(".xaxis").css({ "align": "center", "height": "200px", color: options.labelColor });
-  $(".barTitle").css({ "color": options.titleColor, "font-size": options.titleFontSize + "px" });
+  $("." + prefix + "barChart").css("position", "relative");
+  $("." + prefix + "xaxis").css({ "align": "center", "height": "200px", color: options.labelColor, "padding-top": "90px" });
+  $("." + prefix + "barTitle").css({ "color": options.titleColor, "font-size": options.titleFontSize + "px" });
 
 
-  $(".yaxis").css({
-    "position": "absolute", "margin-left": "-100px", "height": "200px", "transform": "rotate(-90deg)",
+  $("." + prefix + "yaxis").css({
+    "position": "absolute", "margin-left": tickLabelLeftMargin + "px", "height": "200px", "transform": "rotate(-90deg)",
     "margin-top": height / 3 + "px", color: options.labelColor
   });
 
@@ -71,11 +85,11 @@ function drawSingleValuedBarChart(data, options, element) {
   for (let i = 0; i < tickCount; i++) {
 
     let tickDistance = (options.tickValues[i] / maxValue) * height;;
-    $(".hr" + i).css({
+    $("." + prefix + "hr" + i).css({
       "position": "absolute", "width": "40px", "height": "2px", "margin-left": "-20px",
       "margin-top": height - tickDistance + "px", "margin-bottom": "0px"
     })
-    $(".tickValue" + i).css({
+    $("." + prefix + "tickValue" + i).css({
       "position": "absolute", "margin-left": "-55px", "margin-top": height - tickDistance - 10 + "px",
       "margin-bottom": "0px", "width": "40px"
     });
