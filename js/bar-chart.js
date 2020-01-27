af42b9686223ae67c8c4fb41f312cae87408b4be
neethu-ms <msneethu2989@gmail.com>
2020-01-27 17:56:45 +0000

    Added function for drawing multi valued bar chart which can be used for single valued also. Deleted the function for single valued

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,7 +1,8 @@
 
 let barCount = 0;
-// Function to draw single valued bar chart starts
-function drawSingleValuedBarChart(data, options, element) {
+
+
+function drawMultiValuedBarChart(data, options, element) {
   let spaceWidth = options.barSpacing;
   let valueCount = data.length;
   let width = options.width;
@@ -10,27 +11,40 @@ function drawSingleValuedBarChart(data, options, element) {
 
 
   let prefix = "A" + barCount;
-  console.log("Prefix=" + prefix);
+
   barCount++;
 
   let newElement = "<div><h1 class=\"" + prefix + "barTitle\">" + options.barTitle + "</h1><div class=\"" + prefix + "barChart\">";
   newElement += "<h2 class=\"" + prefix + "yaxis\">" + options.yAxis + "</h2>";
 
   let tickCount = options.tickValues.length;
+  let noOfBars = data[0].value.length;
 
   //Creating tick marks using horizontal line
   for (let i = 0; i < tickCount; i++) {
     newElement += "<h2 class=\"" + prefix + "tickValue" + i + "\">" + options.tickValues[i] + "</h2><hr class=\"" + prefix + "hr" + i + "\">";
   }
 
+  if (data[0].value.length > 0) {
+    newElement += "<div class=\"" + prefix + "valueBox flexColumn\">";
+     for (let j = 0; j < noOfBars; j++) {
+      newElement +="<div class=\"flexRow\"><div class=\"" + prefix + "valueIcon"+j+"\"" + "></div><p class=\""+prefix+"barName"+j+"\">"+options.barNames[j]+"</p></div>";
+    }
+    newElement += "</div>"
+  }
+
   // To align elements as a row using the class flexRow
   newElement += "<div class=\"flexRow\">";
 
   //bar Width is computed based on the value count
-  let barWidth = (width - (spaceWidth * valueCount)) / valueCount;
+  let barWidth = 0;
+
+
+  barWidth = ((width - (spaceWidth * valueCount)) / valueCount) / noOfBars;
+
 
   // Finding maximum value so that bar height can be adjusted according to that value
-  let maxValue = getMaxValue(data);
+  let maxValue = getMaxValue(data,options);
   if (maxValue >= 100 && maxValue < 1000) {
     tickLabelLeftMargin = -165;
   } else if (maxValue > 1000) {
@@ -39,9 +53,13 @@ function drawSingleValuedBarChart(data, options, element) {
 
   for (let i = 0; i < data.length; i++) {
     //using flexColumn to arrange elements as a column
-    newElement += "<div class=\"flexColumn\">";
-    newElement += "<div class=\"" + prefix + "bar" + " " + prefix + "bar" + i + "\"><p>" + data[i].value + "</p></div><h2>" +
-      data[i].label + "</h2></div>" + "<div class=\"" + prefix + "space\"></div>";
+    newElement += "<div class=\"flexColumn\"><div class=\"flexRow\">";
+    for (let j = 0; j < noOfBars; j++) {
+      newElement += "<div class=\"" + prefix + "bar" + " " + prefix + "bar" + i + "B" + j + "\"><p>" + data[i].value[j] + "</p></div>";
+    }
+
+    newElement += "</div><h2>" + data[i].label + "</h2></div>" +
+      "<div class=\"" + prefix + "space\"></div>";
   }
   newElement += "</div>";
   newElement += "</div>";
@@ -60,15 +78,24 @@ function drawSingleValuedBarChart(data, options, element) {
   $(".flexColumn").css({ "display": "flex", "flex-direction": "column" });
   $(".flexRow").css({ "display": "flex", "flex-direction": "row" });
 
+  $("."+prefix + "valueBox").css({"position":"absolute","width":"250px","height":"200px",
+  "margin-left":width+50+"px","margin-top":"150px"});
+   for (let j = 0; j < noOfBars; j++) {
+    $("."+prefix+"valueIcon"+j).css({"background-color":options.barColor[j],"width":"20px","height":"20px","margin":"20px"});
+    $("."+prefix+"barName"+j).css({"color":"black","margin":"20px","height":"20px"});
+  }
+
   for (let i = 0; i < data.length; i++) {
 
     //computing bar height based on the maximum value
-    let barHeight = (data[i].value / maxValue) * height;
+    for (let j = 0; j < noOfBars; j++) {
+      let barHeight = (data[i].value[j] / maxValue) * height;
 
-    $("." + prefix + "bar" + i).css({ "height": barHeight + "px", "margin-top": (height - barHeight) + "px" });
+      $("." + prefix + "bar" + i + "B" + j).css({ "height": barHeight + "px", "margin-top": (height - barHeight) + "px", "background-color": options.barColor[j] });
+    }
   }
 
-  $("." + prefix + "bar").css({ "width": barWidth + "px", "background-color": options.barColor, "border": "1px solid black" });
+  $("." + prefix + "bar").css({ "width": barWidth + "px", "border": "1px solid black" });
   $("." + prefix + "space").css("width", spaceWidth + "px");
 
   $("." + prefix + "barChart").css("position", "relative");
@@ -96,36 +123,36 @@ function drawSingleValuedBarChart(data, options, element) {
   }
 
 }
-// Function to draw single valued bar chart ends
-
-function drawMultiValuedBarChart(data, options, element) {
-  return null;
-}
 
 //API for bar chart starts
 function drawBarChart(data, options, element) {
 
-  //Call appropriate function based on the type of bar chart
-  if (options.multiValue === false) {
-    drawSingleValuedBarChart(data, options, element);
-  } else {
     drawMultiValuedBarChart(data, options, element);
-  }
 
 }//API for bar chart ends
 
+
+
 /* Function to find the maximum value of the data starts*/
-function getMaxValue(data) {
+function getMaxValue(data,options) {
   let maxValue;
   for (let i = 0; i < data.length; i++) {
-    if (maxValue === undefined) {
-      maxValue = data[i].value;
-    } else if (data[i].value > maxValue) {
-      maxValue = data[i].value;
+    for (let j = 0; j < data[0].value.length; j++) {
+      if (maxValue === undefined) {
+        maxValue = data[i].value[j];
+      } else if (data[i].value[j] > maxValue) {
+        maxValue = data[i].value[j];
+      }
     }
-
   }
 
+  for(let j=0;j<options.tickValues.length;j++){
+    if (maxValue === undefined) {
+      maxValue = options.tickValues[j];
+    } else if (options.tickValues[j] > maxValue) {
+      maxValue = options.tickValues[j];
+    }
+  }
   return maxValue;
 }
 /* Function to find the maximum value of the data ends*/
